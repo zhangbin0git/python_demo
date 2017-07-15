35efd373b94a8325190dfa40617d28324d1c2963
zhangbin <zhangbin.email@foxmail.com>
2017-07-15 19:21:31 +0800

    first commit

---
+++ Hello-world/Hello_world.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2017/7/15 18:50
+# @Author  : Zhang Bin
+# @Site    : office
+# @File    : Hello_world.py
+# @Software: PyCharm Community Edition
+
+# 屏幕输出Hello world!!!
+print("Hello world!!!")
